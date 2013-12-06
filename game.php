d716f41a403ade1edb917950826daabc453e47df
Diane Deng <didi.deng@gmail.com>
2013-12-05 20:09:27 -0500

    added index & css placeholder files

---
+++ game.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+ 
+ <!--
+ !  Diane Deng
+ !  CSCI E-15
+ !  p3.dianerdeng.com
+ !	12.06.2013 @ 11:59PM
+ -->
+
+ 
+ 
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>CSCI E-15 PROJECT 3</title>
+	<link href="../css/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id ="content">
+		<p><h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h1> Donec ultricies purus eu interdum tincidunt. Aenean rutrum sodales rutrum. Fusce bibendum posuere elit sit amet commodo. Aenean tempor, neque nec ultrices pellentesque, libero lacus porttitor libero, nec porta tellus felis mattis risus. Sed ac lorem a urna adipiscing suscipit. Vestibulum ut dictum erat, non varius nulla. Nulla aliquam nisi vitae orci dapibus fermentum. Maecenas porta sollicitudin mauris id adipiscing. Nullam aliquet neque id lacus pharetra volutpat. Donec bibendum ornare nunc. Nullam tincidunt egestas ligula. Sed mattis magna nunc, eget lacinia lectus pretium non. Cras mi enim, luctus fermentum enim non, auctor vulputate est. Curabitur id nisl at justo molestie blandit id eu dui. Duis aliquam consectetur pretium. Sed in eros nec libero sagittis imperdiet id sed mauris.
+		</p>
+	</div>
+
+	<div id="footer">
+		<p>&copy;2013 Diane Deng | HARVARD DWA15</p>
+	</div>
+</body>
+</html>

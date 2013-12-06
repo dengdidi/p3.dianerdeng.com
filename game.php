ba38894dccc29603c918d55c87ce24f924f1ec05
Diane Deng <didi.deng@gmail.com>
2013-12-06 18:47:03 -0500

    added pop up window for curse generator output

--- game.php
+++ game.php
@@ -6,23 +6,39 @@
  !  p3.dianerdeng.com
  !	12.06.2013 @ 11:59PM
  -->
-
  
  
 <html>
+
 <head>
 	<meta charset="utf-8">
-	<title>CSCI E-15 PROJECT 3</title>
+	<title>Believr: Rumple's Curse Creator</title>
 	<link href="../css/styles.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
 	<div id ="content">
-		<p><h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h1> Donec ultricies purus eu interdum tincidunt. Aenean rutrum sodales rutrum. Fusce bibendum posuere elit sit amet commodo. Aenean tempor, neque nec ultrices pellentesque, libero lacus porttitor libero, nec porta tellus felis mattis risus. Sed ac lorem a urna adipiscing suscipit. Vestibulum ut dictum erat, non varius nulla. Nulla aliquam nisi vitae orci dapibus fermentum. Maecenas porta sollicitudin mauris id adipiscing. Nullam aliquet neque id lacus pharetra volutpat. Donec bibendum ornare nunc. Nullam tincidunt egestas ligula. Sed mattis magna nunc, eget lacinia lectus pretium non. Cras mi enim, luctus fermentum enim non, auctor vulputate est. Curabitur id nisl at justo molestie blandit id eu dui. Duis aliquam consectetur pretium. Sed in eros nec libero sagittis imperdiet id sed mauris.
-		</p>
+		<h1><center>Rumplestilskin's Curse Generator</center></h1>
+		<p>Fill out the form accordingly to generate a curse with Rumplestilskin's help!</p>
+		<p id="demo"></p>
+
+		<center><button type="button" onclick="myCurseWindow()">Create My Curse!</button></center>
+
+		<script>
+			function myCurseWindow()
+			{
+				var myCurse = window.open("","myCurse","width=300,height=300");
+				myCurse.document.write("<p>This is 'Your Curse'. Boo, Begone!</p>");
+				//document.getElementById("demo").innerHTML = Date();
+			}
+		</script>
+
+		<center><small><em><p>Only the truest believr can cast the strongest curses!</p></em></small></center>	
 	</div>
 
 	<div id="footer">
 		<p>&copy;2013 Diane Deng | HARVARD DWA15</p>
 	</div>
 </body>
+
 </html>

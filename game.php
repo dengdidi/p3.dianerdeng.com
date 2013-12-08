a8fa80d4b8676826012670ad3e9402b1bfa99d53
Diane Deng <didi.deng@gmail.com>
2013-12-07 23:52:23 -0500

    added javascript files, trying outvaried methods of form validation

--- game.php
+++ game.php
@@ -8,49 +8,68 @@
  
  
 <html>
-<html lang="en">
 
 <head>
 	<meta charset="utf-8">
 	<title>Believr: Rumple's Curse Generator</title>
+	
 	<link href="../css/styles.css" rel="stylesheet" type="text/css">
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/jquery.form.js"></script>
 
 		<script>
-			 
-			function createCurse(form){
-				
-				alert("Are you sure you REALLY want to curse " + form.fieldName.value + "?" );
+
+			function validateForm()
+        {
+
+            if ( curse.fieldName.value = null || curse.fieldName.value = "")
+            {
+              alert("Please enter all fields");
+              return false;
+            }
+            else
+            {
+
+             return true;
+            } 
+        }
+
+			function createCurse(curse){
+
+				alert("Are you sure you REALLY want to curse " + curse.fieldName.value + "?" );
 
 				var curseLines;
 
-					curseLines = ("<center><b>The Curse of " + form.fieldName.value + "</b><br><br></center>");
+					curseLines = ("<center><b>The Curse of " + curse.fieldName.value + "</b><br><br></center>");
 					curseLines += ("<p><em>You'll Need:</em>");
-					curseLines += ("<ul><li>"+ form.field1.value + " tsp. of " + form.field2.value + " dust</li>");
-					curseLines += ("<li>1 cup of " + form.field3.value + "</li>");
-					curseLines += ("<li>2 drops of " + form.field4.value + " ink</li>");
-					curseLines += ("<li>" + form.field5.value + " troll toe nail clippings</li>");
-					curseLines += ("<li>3 cups of " + form.field6.value + " juice</li></ul>");
-					curseLines += ("Stir for " + form.field7.value + " strokes in large iron mixing bowl & set aside for at least 1 hour before using</p><br>");
+					curseLines += ("<ul><li>"+ curse.field1.value + " tsp. of " + curse.field2.value + " dust</li>");
+					curseLines += ("<li>1 cup of " + curse.field3.value + "</li>");
+					curseLines += ("<li>2 drops of " + curse.field4.value + " ink</li>");
+					curseLines += ("<li>" + curse.field5.value + " troll toe nail clippings</li>");
+					curseLines += ("<li>3 cups of " + curse.field6.value + " juice</li></ul>");
+					curseLines += ("Stir for " + curse.field7.value + " strokes in large iron mixing bowl & set aside for at least 1 hour before using</p><br>");
 					curseLines += ("<p><em>Curse:</em></p>");
-					curseLines += ("<p>May " + form.field8.value + " " + form.field9.value + " " + form.fieldName.value + " in the " + form.field10.value + ". And for " + form.field11.value + " years, " + form.fieldName.value + " shall " + form.field12.value + " in " + form.field13.value + " where " + form.field14.value + " " + form.field15.value + " " + form.fieldName.value + " until " + form.fieldName.value + " " + form.field16.value + ". I call upon Rumplestilskin and Pandora's box to hex " + form.fieldName.value + " for " + form.field17.value + " years.</p>");			
+					curseLines += ("<p>May " + curse.field8.value + " " + curse.field9.value + " " + curse.fieldName.value + " in the " + curse.field10.value + ". And for " + curse.field11.value + " years, " + curse.fieldName.value + " shall " + curse.field12.value + " in " + curse.field13.value + " where " + curse.field14.value + " " + curse.field15.value + " " + curse.fieldName.value + " until " + curse.fieldName.value + " " + curse.field16.value + ". I call upon Rumplestilskin and Pandora's box to hex " + curse.fieldName.value + " for " + curse.field17.value + " years.</p>");			
 					
 				curseWindow = window.open("","displayWindow","toolbar=no,width=300,height=500,directories=no,status=no,scrollbars=no,resize=yes,menubar=no")
 				curseWindow.document.write(curseLines)
 				curseWindow.document.close()
 			}
 		</script>
+
+		<noscript>Sorry, your browser does not support JavaScript!</noscript>
 	
 </head>
 
 <body>
 	<div id = "content">
-		<center>
 			<h1>Rumplestilskin's Curse Generator</h1>
 			<p>Fill out the form carefully to generate a curse with Rumplestilskin's help.</p>
-			<small><em><p>*Only the truest believr can cast the strongest curses!</p></em></small><br>
+			<p><small><em>*Only the truest believr can cast the strongest curses!</em></small></p><br>
 
-		<form name = "curse">
+		<form action="" name = "curse" onsubmit="return validateForm()">
+			<center>
 			<table border="0">
 
 				<tr>
@@ -142,15 +161,13 @@
 					<td>Number:</td>
 					<td><input type="text" name="field17" size="30"></td>
 				</tr>
-
 			</table>
 
 			<br>
 			
 			<input type="button" value="Get Your Curse Now!" onclick="createCurse(this.form)">	
-		
+			</center>
 		</form>
-		</center>
 
 		<br><br>
 		<p>
